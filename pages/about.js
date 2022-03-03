dd873a17f2c067cb363ed75c171fd6bab3b973d7
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-03-03 16:51:35 -0600

    Primera pagina

--- pages/about.js
+++ pages/about.js
@@ -1,25 +1,28 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function HomePage() {
+const AboutPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Home - Oscar</title>
+        <title>About - Oscar</title>
         <meta name="description" content="Home page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Ir a <a href="/about">About</a>
+          Ir a <a href="/">Home</a>
         </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <code className={styles.code}>pages/about.js</code>
         </p>
       </main>
     </div>
   )
 }
+
+export default AboutPage
