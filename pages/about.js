76be834af74b96a19c566da19f5e11c636556f52
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-03-06 09:23:51 -0600

    Multiples layouts anidados

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
 import { MainLayout } from '../components/layouts/MainLayout';
+import { DarkLayout } from '../components/layouts/DarkLayout';
 
 const AboutPage = () => {
   return (
-    <MainLayout title="About">
+    <>
       <h1>About Page</h1>
 
       <h2 className="title">
@@ -14,6 +15,16 @@ const AboutPage = () => {
       <p className="description">
         Get started by editing <code className="code">pages/about.js</code>
       </p>
+    </>
+  );
+};
+
+AboutPage.getLayout = function getLayout(page) {
+  return (
+    <MainLayout title="About">
+      <DarkLayout>
+        {page}
+      </DarkLayout>
     </MainLayout>
   );
 };
